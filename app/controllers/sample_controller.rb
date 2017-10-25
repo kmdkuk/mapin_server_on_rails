1cdf30a7329c5c6b4da8683b5ebba630cead20d0
kouki <kouworld0123@gmail.com>
2017-10-25 17:05:27 +0900

    root変えた

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -2,11 +2,5 @@ require 'json'
 
 class SampleController < ApplicationController
   def index
-    filePath = "#{Rails.root}/sample.json"
-    sample = []
-    File.open(filePath) do |file|
-      sample = JSON.load(file)
-    end
-    render json: JSON.pretty_generate(sample)
   end
 end
