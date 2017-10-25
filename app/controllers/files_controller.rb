712549295af25e717d45969286b59051e1112bf6
kouki <kouworld0123@gmail.com>
2017-10-25 15:31:04 +0900

    緯度経度をfiles/?latitude=xxx.xxxx&longitude=xxx.xxxで受け取って処理するようにした パラメータを受け取れた場合, 誤差0.01の範囲にあるファイルを表示

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,13 +1,26 @@
 require 'json'
 class FilesController < ApplicationController
   def index
+    longitude = params[:longitude].to_f
+    latitude = params[:latitude].to_f
+    geocodeFlag = false
+    geocodeFlag = true if params[:longitude].present? && params[:latitude].present?
     companies = Company.all
-    result = {status: "200", companies: []}
+    result = {status: "200", longitude: longitude, latitude: latitude, flag: geocodeFlag, companies: []}
     companies.each_with_index do |company, i|
       result[:companies].push({info: company})
       result[:companies][i][:shops] = []
       shops = company.shops.all
       shops.each_with_index do |shop, j|
+        if geocodeFlag then
+          shop_longitude = 0.0
+          shop_latitude = 0.0
+          shop_longitude = shop[:longitude]
+          shop_latitude = shop[:latitude]
+          if ((shop_longitude - longitude).abs > 0.01 && (shop_latitude - latitude).abs > 0.01)
+            next
+          end
+        end
         shop_json = shop.to_json
         shop_hash = JSON.parse(shop_json)
         result[:companies][i][:shops].push({info: shop_hash})
@@ -19,7 +32,9 @@ class FilesController < ApplicationController
           result[:companies][i][:shops][j][:files].push(file)
         end
       end
+      result[:companies][i] = nil if result[:companies][i][:shops].empty?
     end
+    result[:companies].compact!
     render json: result
   end
 end
