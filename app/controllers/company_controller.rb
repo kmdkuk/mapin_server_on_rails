5692396e8e12c3843cf6f7501db5fd6ca3f56a36
kouki <kouworld0123@gmail.com>
2017-10-29 16:54:08 +0900

    g controller company

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -2,4 +2,9 @@ class CompanyController < ApplicationController
   def index
     @companies = Company.all
   end
+
+  def show
+    @company = Company.find(params[:id])
+    @shops = @company.shops
+  end
 end
