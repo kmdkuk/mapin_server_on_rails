fcbcbc3502526067bb8d787cf33585da31615133
kouki <kouworld0123@gmail.com>
2017-10-30 16:16:46 +0900

    コントロール半分作成

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -3,4 +3,23 @@ class ShopsController < ApplicationController
     @company = Company.find(params[:company_id])
     @shops = @company.shops
   end
+
+  def show
+    @company = Company.find(params[:company_id])
+    @shop = @company.shops.find(params[:id])
+  end
+
+  def new
+    @company = Company.find(params[:company_id])
+    @shop = @company.shops.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
