defff3a1a394e71c3c57f38d8c9d7ffb8916fd03
kouki <kouworld0123@gmail.com>
2017-11-02 00:57:48 +0900

    shopコントローラー完成

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -11,15 +11,41 @@ class ShopsController < ApplicationController
 
   def new
     @company = Company.find(params[:company_id])
-    @shop = @company.shops.new
+    @shop = @company.shops.build
   end
 
   def create
+    @shop = @company.shops.build(shop_params)
+    if @shop.save
+      flash[:success] = "Shop add!"
+      redirect_to @shop
+    else
+      flash[:danger] = "Company add fail..."
+      render 'new'
+    end
   end
 
   def edit
+    @company = Company.find(params[:company_id])
+    @shop = @company.shops.find(params[:id])
   end
 
   def update
+    @company = Company.find(params[:company_id])
+    @shop = @company.shops.find(params[:id])
+    if @shop.update_attributes(shop_params)
+      flash[:success] = "Shop data updated"
+      redirect_to [@company, @shop]
+    else
+      flash[:danger] = "Update failed..."
+      render 'edit'
+    end
   end
+
+  private
+    def shop_params
+      params.require(:shop).permit(:name, :image_url, :postal_code,
+                                   :address, :tel, :hours_begin, :hours_end,
+                                   :longitude, :latitude)
+    end
 end
