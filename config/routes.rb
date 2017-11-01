657a9a88749cf14e66048d5bc6d1465e7cd84feb
kouki <kouworld0123@gmail.com>
2017-11-02 01:18:24 +0900

    ルーティングを適用

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'files#index'
+  root 'data_formats#index'
 
   resources :companies do
     resources :shops do
