ed56318c5c6ad2d15cd78d404dd8e23445907235
kouki kamada <kouworld0123@gmail.com>
2017-11-01 18:09:35 +0000

    問題があったのでローカルにあるサンプルをそのまま返却

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -5,9 +5,10 @@ class SampleController < ApplicationController
   def index
     filePath = "#{Rails.root}/sample.json"
     sample = []
-    sample = open("https://raw.githubusercontent.com/miraikeitai2017/mapin-All/master/Json-Sample/sample.json?token=APLozRHpn2H4NjTgMCaI1OZtoryPXkN_ks5Z8CpDwA%3D%3D", &:read)
+    # githubにあるjsonサンプルがprivateだからかtokenが有効切れるのでだめだった
+    # sample = open("https://github.com/miraikeitai2017/mapin-All/raw/master/Json-Sample/sample.json", &:read)
     File.open(filePath) do |file|
-      # sample = JSON.load(file)
+      sample = JSON.load(file)
     end
     render json: sample
   end
