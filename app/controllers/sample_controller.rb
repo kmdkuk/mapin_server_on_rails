1726fabb2c4c4ac7a3bda75c5a71c6f22919b8e3
kouki kamada <kouworld0123@gmail.com>
2017-11-01 18:01:47 +0000

    sampleデータをマッピンオールから取得してそれをそのまま帰すプログラム

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -1,6 +1,14 @@
 require 'json'
+require "open-uri"
 
 class SampleController < ApplicationController
   def index
+    filePath = "#{Rails.root}/sample.json"
+    sample = []
+    sample = open("https://raw.githubusercontent.com/miraikeitai2017/mapin-All/master/Json-Sample/sample.json?token=APLozRHpn2H4NjTgMCaI1OZtoryPXkN_ks5Z8CpDwA%3D%3D", &:read)
+    File.open(filePath) do |file|
+      # sample = JSON.load(file)
+    end
+    render json: sample
   end
 end
