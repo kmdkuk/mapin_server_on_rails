b771fcb05ab69d36e3748e1056d1d6b6a632cf99
kouki <kouworld0123@gmail.com>
2017-11-06 05:04:10 +0900

    companyもjoinに追加

--- app/controllers/api/shops_controller.rb
+++ app/controllers/api/shops_controller.rb
@@ -1,11 +1,11 @@
 class Api::ShopsController < ApplicationController
   def index
-    @shops = Shop.joins(:category).select("shops.*, categories.name as category_name").all
+    @shops = Shop.joins(:category, :company).select("shops.*, companies.name as company_name, categories.name as category_name").all
     render json: @shops
   end
 
   def show
-    @shop = Shop.joins(:category).select("shops.*, categories.name as category_name").find(params[:id])
+    @shop = Shop.joins(:category, :company).select("shops.*, companies.name, categories.name as category_name").find(params[:id])
     render json: @shop
   end
 end
