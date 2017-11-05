e73cc4bba9f23a5fba40cba0ac111168a06d15ca
kouki <kouworld0123@gmail.com>
2017-11-06 05:01:32 +0900

    shopsにcategoryをjoin

--- app/controllers/api/shops_controller.rb
+++ app/controllers/api/shops_controller.rb
@@ -1,11 +1,11 @@
 class Api::ShopsController < ApplicationController
   def index
-    @shops = Shop.all
+    @shops = Shop.joins(:category).select("shops.*, categories.name as category_name").all
     render json: @shops
   end
 
   def show
-    @shop = Shop.find(params[:id])
+    @shop = Shop.joins(:category).select("shops.*, categories.name as category_name").find(params[:id])
     render json: @shop
   end
 end
