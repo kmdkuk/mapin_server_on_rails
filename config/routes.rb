38a84f7f5a1ce73ea507548cd6efcb0b8dee3018
kouki <kouworld0123@gmail.com>
2017-11-06 03:16:17 +0900

    typo

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'data_formats#index'
   get '/sample', to: 'sample#index'
+
   resources :companies do
     resources :shops do
       resources :uploaded_files
