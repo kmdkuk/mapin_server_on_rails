bde4922e0d070fcd20774c39b1147b10ecc6f118
kouki <kouworld0123@gmail.com>
2017-11-08 00:37:29 +0900

    Revert "GoogleMapを使う準備"
    This reverts commit 28b3a1b47a8b70ade6b11550476dd89e620a1a22.

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,14 +2,4 @@ class Shop < ApplicationRecord
   has_many :uploaded_files
   belongs_to :company
   belongs_to :category
-
-  before_save :geocode_full_address
-
-  def geocode_full_address
-    coords = Geocoder.coordinates(
-      self.prefecture + self.city + self.address # 県名 + 市町村名 + 丁目番地
-    )
-    self.latitude = coords[0]
-    self.longitude = coords[1]
-  end
 end
