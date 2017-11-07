f173b7999e06d5efaa9b2173cb69726191b659fb
kouki kamada <kouworld0123@gmail.com>
2017-11-07 18:00:34 +0000

    /sampleのルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'companies#index'
-  get '/sample', to: 'sample#index'
-
+  get '/sample', to: 'data_formats#index'
   resources :companies do
     resources :shops do
       resources :files, controller: :uploaded_files
