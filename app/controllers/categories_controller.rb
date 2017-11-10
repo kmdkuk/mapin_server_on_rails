b652e481554837d16fcadca8247f6041d34db0ba
kouki <kouworld0123@gmail.com>
2017-11-10 10:07:33 +0900

    コントローラー大方つくった

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,17 +4,41 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @category = Category.find(params[:id])
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Category create!"
+      redirect_to @category
+    else
+      flash[:danger] = "Category create fail..."
+      render 'new'
+    end
   end
 
   def edit
+    @category = Category.find(params[:id])
   end
 
   def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      flash[:success] = "category updated!"
+      redirect_to @category
+    else
+      flash[:danger] = "category update fail..."
+      render 'edit'
+    end
   end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
