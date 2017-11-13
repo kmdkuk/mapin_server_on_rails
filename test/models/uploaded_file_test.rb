d0d1a03eb66d3776d316189e6ca8bee00211efed
kouki <kouworld0123@gmail.com>
2017-11-13 14:26:47 +0000

    model/uploaded_filのテスト

--- test/models/uploaded_file_test.rb
+++ test/models/uploaded_file_test.rb
@@ -3,6 +3,20 @@ require 'test_helper'
 class UploadedFileTest < ActiveSupport::TestCase
   def setup
     @shop = shops(:one)
-    @file = @shop.uploaded_files.build(name: "testfile", url: "http://testfile", file_type: "pdf")
+    @file = @shop.uploaded_files.build()
+  end
+  
+  test "should be valid" do
+    assert @file.valid?
+  end
+  
+  test "name should be present" do
+    @file.name = ""
+    assert_not @file.valid?
+  end
+  
+  test "file_type should be present" do
+    @file.file_type = ""
+    assert_not @file.valid?
   end
 end
