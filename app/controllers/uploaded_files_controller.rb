459d1fdbdf5d1a6f70c87c710cebef1cadb058db
kouki <kouworld0123@gmail.com>
2017-11-13 14:46:22 +0000

    filename.file_typeでファイル表現できるように変更

--- app/controllers/uploaded_files_controller.rb
+++ app/controllers/uploaded_files_controller.rb
@@ -31,9 +31,11 @@ class UploadedFilesController < ApplicationController
   def create
     @company = Company.find(params[:company_id])
     @shop = @company.shops.find(params[:shop_id])
-    params[:uploaded_file][:name] = params[:uploaded_file][:file].original_filename # if params[:uploaded_file][:name].empty?
     params[:uploaded_file][:file_type] = File.extname(params[:uploaded_file][:file].original_filename)
+    params[:uploaded_file][:name] = params[:uploaded_file][:file].original_filename if params[:uploaded_file][:name].empty?
+    params[:uploaded_file][:name].slice!(params[:uploaded_file][:file_type])
     params[:uploaded_file][:file_type].slice!(0)
+    
     @file = @shop.uploaded_files.build(file_params)
     if @file.save
       flash[:success] = "Shop add!"
