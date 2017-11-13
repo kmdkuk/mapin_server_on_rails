28e558484141726c73f4e96498f2634b70734947
kouki <kouworld0123@gmail.com>
2017-11-13 06:46:18 +0000

    model/shopのテストのセットアップ

--- test/models/shop_test.rb
+++ test/models/shop_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class ShopTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @company = companies(:one)
+    @shop = @company.shops.build(name: "Example shop", image_url: "testURL",
+                          postal_code: "000-0000", address: "北海道札幌市北区北18条西6丁目",
+                          tel: "090-0000-0000", business_hours: "24時間営業",
+                          homepage: "testURL", category: Category.first)
+  end
   
+  test "should be valid" do
+    assert @shop.valid?
+  end
 end
