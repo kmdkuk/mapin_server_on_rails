615bb7e60df46d912b8e00013d8641c3fd7de740
kouki <kouworld0123@gmail.com>
2017-11-13 07:24:43 +0000

    model/shopの存在確認だけ

--- test/models/shop_test.rb
+++ test/models/shop_test.rb
@@ -6,10 +6,35 @@ class ShopTest < ActiveSupport::TestCase
     @shop = @company.shops.build(name: "Example shop", image_url: "testURL",
                           postal_code: "000-0000", address: "北海道札幌市北区北18条西6丁目",
                           tel: "090-0000-0000", business_hours: "24時間営業",
-                          homepage: "testURL", category: Category.first)
+                          homepage: "testURL", category_id: Category.first.id)
   end
   
   test "should be valid" do
     assert @shop.valid?
+  end
+  
+  test "name should be present" do
+    @shop.name = " "
+    assert_not @shop.valid?
+  end
+  
+  test "image_url should be present" do
+    @shop.image_url = ""
+    assert_not @shop.valid?
+  end
+  
+  test "postal_code should be present" do
+    @shop.postal_code = ""
+    assert_not @shop.valid?
+  end
+  
+  test "address should be present" do
+    @shop.address = ""
+    assert_not @shop.valid?
+  end
+  
+  test "tel should be present" do
+    @shop.tel = ""
+    assert_not @shop.valid?
   end
 end
