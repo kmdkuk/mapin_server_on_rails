fe6789e203660de22a412d1f818cb61872a71d26
kouki <kouworld0123@gmail.com>
2017-11-15 01:07:43 +0900

    nested route use shallow

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'companies#index'
   get '/sample', to: 'data_formats#index'
-  resources :companies do
+  resources :companies, shallow: true do
     resources :shops do
-      resources :files, controller: :uploaded_files
+      resources :files, controller: :uploaded_files do
+        get 'download', to: 'uploaded_files#download'
+      end
     end
-  end
-
-  resources :files, controller: :uploaded_files do
-    get '/download', to: 'uploaded_files#download'
   end
 
   resources :categories
