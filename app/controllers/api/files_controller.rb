0f0dc41ed7d15bb89bd0df5c90fd4b2423f0814a
kouki kamada <kouworld0123@gmail.com>
2017-11-20 14:17:42 +0000

    withinの使い方直した

--- app/controllers/api/files_controller.rb
+++ app/controllers/api/files_controller.rb
@@ -1,9 +1,9 @@
 class Api::FilesController < ApplicationController
   def index
-    if params[:longitude].present? && params[:latitude].present? && !params[:shop_id]
-      longitude = params[:longitude].to_f
-      latitude = params[:latitude].to_f
-      shops = Shop.select(:id).within(1,[latitude, longitude])
+    longitude = params[:longitude].to_f
+    latitude = params[:latitude].to_f
+    if latitude.instance_of?(Float) && longitude.instance_of?(Float) && !params[:shop_id]
+      shops = Shop.select(:id).within(1, origin: [latitude, longitude])
       @files = UploadedFile.where(shop_id: shops)
     elsif params[:shop_id]
       shops = Shop.find(params[:shop_id])
