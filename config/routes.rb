ed73ac88bdb9c774d457ab0356c9747c63b64814
kouki kamada <kouworld0123@gmail.com>
2017-11-24 06:51:44 +0000

    /wsgi作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'companies#index'
-  get '/sample', to: 'data_formats#index'
+  get '/wsgi', to: 'data_formats#index'
   resources :companies, shallow: true do
     resources :shops do
       resources :files, controller: :uploaded_files do
